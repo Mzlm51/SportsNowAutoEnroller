92a3753451564617d080cc0bda81c7f7af46d8d8
Mazlum Oyman <moyman@student.ethz.ch>
2026-04-05 15:42:43 +0200

    fix

--- main.py
+++ main.py
@@ -38,8 +38,9 @@ def init():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--single-process")
+    options.add_argument("--no-zygote")
     options.add_argument("--disable-extensions")
+    options.add_argument("--window-size=1920,1080")
     options.binary_location = "/usr/bin/chromium"
     service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=options)
